5b502780d6eb6463d9642ee81eabb14a9a0a0564
alayna0049 <aschwartz440@gmail.com>
2025-09-08 22:36:42 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class HelloTwist
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("What is your name?");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("What is your favorite hobby?");
+        string hobby = Console.ReadLine();
+
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~");
+
+        Console.WriteLine($"Hello, {name}!");
+
+        Console.WriteLine($"It's awesome that you like {hobby}. {hobby} is so diverse!");
+
+        Console.WriteLine($"Did you know that {hobby} can be a form of expression?");
+
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~");
+    }
+}
+
+
